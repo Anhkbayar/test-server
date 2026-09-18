2b3b6281f833a41c9adb410c9789f7c01076062e
З.Анхбаяр <B232270029@must.edu.mn>
2026-09-18 22:59:14 +0800

    lab3 nemelt api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,6 +12,23 @@ app.get('/delay', (req, res) => {
     res.send('Response after 0.3 second');
   }, 300);
 });
+
+
+//lab3
+app.post('/cart/add', (req, res) => {
+
+  res.json({ ok: true, items: 1 })
+});
+
+app.get('/report', async (req, res) => {
+  await sleep(200 + Math.random() * 200);
+  res.send({ rows: 20000 });
+})
+
+app.post('/pay', (req, res) => {
+  if (Math.random() < 0.05) return res.status(500).json({ error: 'gateway timeout' });
+  res.json({ paid: true });
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
